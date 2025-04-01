fa2a3057ea513de1fb5ca671e4793e261c291fd0
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-04-01 20:19:38 -0300

    Subindo Projeto

---
+++ src/main/java/com/api/mf_semi_joias/MfSemiJoiasApplication.java
@@ -0,0 +1,13 @@
+package com.api.mf_semi_joias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfSemiJoiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfSemiJoiasApplication.class, args);
+	}
+
+}
