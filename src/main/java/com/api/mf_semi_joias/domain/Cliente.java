ab3d01d46a52140ef7bbeb44203d39aeeadf2ec4
Flavio <samus.sp@gmail.com>
2025-04-01 22:20:45 -0300

    adicionado classe cliente

--- src/main/java/com/api/mf_semi_joias/domain/Cliente.java
+++ src/main/java/com/api/mf_semi_joias/domain/Cliente.java
@@ -1,4 +1,11 @@
 package com.api.mf_semi_joias.domain;
 
 public class Cliente {
+    private Long id;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
+
+
 }
