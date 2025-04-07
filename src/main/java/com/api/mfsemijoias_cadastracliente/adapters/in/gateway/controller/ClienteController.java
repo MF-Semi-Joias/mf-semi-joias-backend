0f76b01c101afa7fb960675fbbaa1a8b1b34e75e
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-04-06 23:23:11 -0300

    Implemntando Controller

--- src/main/java/com/api/mfsemijoias_cadastracliente/adapters/in/gateway/controller/ClienteController.java
+++ src/main/java/com/api/mfsemijoias_cadastracliente/adapters/in/gateway/controller/ClienteController.java
@@ -1,4 +1,49 @@
 package com.api.mfsemijoias_cadastracliente.adapters.in.gateway.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.api.mfsemijoias_cadastracliente.ports.in.ClienteService;
+import com.api.mfsemijoias_cadastracliente.domain.model.Cliente;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
+        clienteService.cadastrarCliente(cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id, @RequestBody Cliente cliente) {
+        clienteService.atualizarCliente(id, cliente);
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
+        clienteService.deletarCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> listarClientes() {
+        List<Cliente> clientes = clienteService.listarClientes();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> buscarClientePorId(@PathVariable Long id) {
+        Cliente cliente = clienteService.buscarClientePorId(id);
+        return ResponseEntity.ok(cliente);
+    }
 }
