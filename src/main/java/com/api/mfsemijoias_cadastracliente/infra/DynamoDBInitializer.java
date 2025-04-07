4ec1e9a40bfaf0f6ecd6593f742239ba888f7508
Flavio <samus.sp@gmail.com>
2025-04-07 17:50:50 -0300

    Criando tabela dynamodb no localstack

---
+++ src/main/java/com/api/mfsemijoias_cadastracliente/infra/DynamoDBInitializer.java
@@ -0,0 +1,34 @@
+package com.api.mfsemijoias_cadastracliente.infra;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.model.*;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DynamoDBInitializer {
+
+    private final AmazonDynamoDB dynamoDB;
+
+    public DynamoDBInitializer() {
+        this.dynamoDB = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("mock_access_key", "mock_secret_key")))
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "sa-east-1"))
+                .build();
+    }
+
+    @PostConstruct
+    public void init() {
+        CreateTableRequest request = new CreateTableRequest()
+                .withTableName("clientes")
+                .withKeySchema(new KeySchemaElement("id", KeyType.HASH))
+                .withAttributeDefinitions(new AttributeDefinition("id", ScalarAttributeType.N))
+                .withBillingMode(BillingMode.PAY_PER_REQUEST);
+
+        dynamoDB.createTable(request);
+    }
+}
