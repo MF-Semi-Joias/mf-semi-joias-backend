c4e9878856d635904e676ddd042deddc1354418d
Flavio <samus.sp@gmail.com>
2025-04-29 00:12:22 -0300

    implementação das camadas de dominio entidade request e response

---
+++ src/main/java/com/api/mfsemijoias_cadastracliente/adapters/in/request/ProdutoRequest.java
@@ -0,0 +1,22 @@
+package com.api.mfsemijoias_cadastracliente.adapters.in.request;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoRequest {
+
+    private String id;
+    private String nome;
+    private String descricao;
+    private String categoria;
+    private String preco;
+    private String imagemUrl;
+    private String quantidadeEstoque;
+    private String dataCadastro;
+    private String dataAtualizacao;
+}
