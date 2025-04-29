04db39d40df3b6aa4751de31f0a63a21f2573869
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-04-29 11:20:16 -0300

    Implementação Security

--- src/main/java/com/api/mfsemijoias_cadastracliente/adapters/in/secutity/SecurityConfig.java
+++ src/main/java/com/api/mfsemijoias_cadastracliente/adapters/in/secutity/SecurityConfig.java
@@ -3,19 +3,28 @@ package com.api.mfsemijoias_cadastracliente.adapters.in.secutity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable()) // Desabilita CSRF usando o novo padrão
-            .authorizeHttpRequests(auth -> {
-                auth.requestMatchers("/clientes/**").permitAll(); // Permite acesso público aos endpoints de clientes
-                auth.anyRequest().permitAll(); // Permite acesso público a qualquer outro endpoint
-            });
+        http.csrf(csrf -> csrf.disable()) // Desabilita CSRF
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/clientes/**").authenticated(); // Protege os endpoints de clientes
+                    auth.anyRequest().permitAll(); // Permite acesso público a outros endpoints
+                })
+                .httpBasic(httpbasic -> {}); // Habilita autenticação HTTP Basic
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Configura o encoder de senhas
     }
 }
