7cc60d4bfa8265a13d7c3d03da303ea76bf72be4
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-05-27 00:48:49 -0300

    Implementando Produto Validation

--- src/main/java/com/api/mfsemijoias_cadastracliente/ports/in/produto/ProdutoValidation.java
+++ src/main/java/com/api/mfsemijoias_cadastracliente/ports/in/produto/ProdutoValidation.java
@@ -1,4 +1,23 @@
 package com.api.mfsemijoias_cadastracliente.ports.in.produto;
 
+import com.api.mfsemijoias_cadastracliente.domain.model.Produto;
+
 public interface ProdutoValidation {
+
+    boolean produtoValido(Produto produto);
+
+    boolean isExistProduto(Produto produto);
+
+    boolean isExistIdProduto(Produto produto);
+
+    boolean isExistNomeProduto(Produto produto);
+
+    boolean isExistDescricaoProduto(Produto produto);
+
+    boolean isExistValorProduto(Produto produto);
+
+    boolean isExistQuantidadeProduto(Produto produto);
+
+    boolean isExistImagemProduto(Produto produto);
+
 }
