c6ba65bb7260533ec43d8f1e3278edb51993df06
Flavio <samus.sp@gmail.com>
2025-05-29 11:15:46 -0300

    commit inicial gateway

---
+++ demomf-semi-joias-backend-gateway/src/main/java/mf_semi_joias_backend/demomf_semi_joias_backend_gateway/DemomfSemiJoiasBackendGatewayApplication.java
@@ -0,0 +1,13 @@
+package mf_semi_joias_backend.demomf_semi_joias_backend_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomfSemiJoiasBackendGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomfSemiJoiasBackendGatewayApplication.class, args);
+	}
+
+}
---
+++ demomf-semi-joias-backend-gateway/src/test/java/mf_semi_joias_backend/demomf_semi_joias_backend_gateway/DemomfSemiJoiasBackendGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package mf_semi_joias_backend.demomf_semi_joias_backend_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemomfSemiJoiasBackendGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
